ed8487d3790800bb3474ace602ffe571868aa534
joaquin-osorio <joaquinosorio55@gmail.com>
2025-07-30 15:24:31 -0300

    Feat: Added VideoBanner.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ import YouTubeEmbed from "@/components/YoutubeEmbed";
 import InfoSection from "@/components/InfoSection";
 import { keylab88Specs, keylab49And61Specs } from "@/data/specs";
 import Specifications from "@/components/Specifications";
+import VideoBanner from "@/components/VideoBanner";
+import video from "@/pu"
 
 export default function Home() {
   return (
@@ -20,6 +22,7 @@ export default function Home() {
         <Hero/>
         <YouTubeEmbed url="https://www.youtube.com/watch?v=-Tmp2mQ7Wfs"/>
         <InfoSection/>
+        <VideoBanner src="/assets/render-loop.mp4" heightClass="h-[500px]"/>
         <Specifications title='Keylab 88 Mk3' specs={keylab88Specs}/>
         <Specifications title='Keylab 49/61 Mk3' specs={keylab49And61Specs}/>
       </main>
---
+++ src/components/VideoBanner.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+export interface VideoBannerProps {
+  src: string;
+  heightClass?: string;
+  poster?: string;
+}
+
+export default function VideoBanner({
+  src,
+  heightClass = "h-96",
+  poster,
+}: VideoBannerProps): JSX.Element {
+  return (
+    <div className={`w-full overflow-hidden relative ${heightClass}`}>
+      <video
+        src={src}
+        poster={poster}
+        autoPlay
+        loop
+        muted
+        playsInline
+        className="w-full h-full object-cover"
+      />
+    </div>
+  );
+}
