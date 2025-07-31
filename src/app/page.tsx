5124d3a7b47571f26aa383d9f46235d79d149a86
joaquin-osorio <joaquinosorio55@gmail.com>
2025-07-31 11:16:02 -0300

    Feat: Added image and footer section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,12 @@ import InfoSection from "@/components/InfoSection";
 import { keylab88Specs, keylab49And61Specs } from "@/data/specs";
 import Specifications from "@/components/Specifications";
 import VideoBanner from "@/components/VideoBanner";
-import video from "@/pu"
 import Gallery from "@/components/Gallery"
 
 
 export default function Home() {
   return (
-    <div className="bg-gray-100 min-h-screen">
+    <div className="bg-gray-100 min-h-screen overflow-x-hidden">
       <header className="relative w-full h-30 bg-white">
         <Image
           src="/assets/arturia-logo.svg"
@@ -27,14 +26,28 @@ export default function Home() {
         <VideoBanner src="/assets/render-loop.mp4" heightClass="h-[500px]"/>
         <Specifications title='Keylab 88 Mk3' specs={keylab88Specs}/>
         <Specifications title='Keylab 49/61 Mk3' specs={keylab49And61Specs}/>
-        <div className="w-full h-96 flex justify-center">
+        <div className="w-full h-96 flex justify-center my-20">
           <Gallery 
             images={['/assets/49_Mk3-no-bg.png','/assets/61_Mk3-no-bg.png','/assets/88_Mk3-no-bg.png']}
             titles={['Keylab 49 Mk3', 'Keylab 61 Mk3', 'Keylab 88 Mk3']}
             links={['#','#','#']}
           />
         </div>
+            <div className='w-full h-[40rem] relative overflow-hidden'>
+                <Image
+                    src="/assets/keylab-closeup.jpg"
+                    alt="Banner principal"
+                    fill
+                    className='object-cover'
+                    quality={100}
+                />
+            </div>
       </main>
+      <footer className="w-full h-32 bg-black text-gray-50 flex items-center justify-center">
+          <div className="text-center text-md font-[supply-light]">
+            PC Midi Center distribuidor oficial de Arturia en Argentina
+          </div>
+      </footer>
     </div>
   );
 }
