4b24b5bfafb6c75a7ffd7c8b2f474b1899edaaa5
joaquin-osorio <joaquinosorio55@gmail.com>
2025-07-31 12:51:31 -0300

    Feat: Added title and favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,47 +6,51 @@ import { keylab88Specs, keylab49And61Specs } from "@/data/specs";
 import Specifications from "@/components/Specifications";
 import VideoBanner from "@/components/VideoBanner";
 import Gallery from "@/components/Gallery"
+import Head from "next/head";
 
 
 export default function Home() {
   return (
     <div className="bg-gray-100 min-h-screen overflow-x-hidden">
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head> 
       <header className="relative w-full h-30 bg-white">
         <Image
           src="/assets/arturia-logo.svg"
           alt="Banner principal"
-          fill 
+          fill
           className="py-5"
         />
       </header>
       <main className="">
-        <Hero/>
-        <YouTubeEmbed url="https://www.youtube.com/watch?v=-Tmp2mQ7Wfs"/>
-        <InfoSection/>
-        <VideoBanner src="/assets/render-loop.mp4" heightClass="h-[500px]"/>
-        <Specifications title='Keylab 88 Mk3' specs={keylab88Specs}/>
-        <Specifications title='Keylab 49/61 Mk3' specs={keylab49And61Specs}/>
+        <Hero />
+        <YouTubeEmbed url="https://www.youtube.com/watch?v=-Tmp2mQ7Wfs" />
+        <InfoSection />
+        <VideoBanner src="/assets/render-loop.mp4" heightClass="h-[500px]" />
+        <Specifications title='Keylab 88 Mk3' specs={keylab88Specs} />
+        <Specifications title='Keylab 49/61 Mk3' specs={keylab49And61Specs} />
         <div className="w-full h-96 flex justify-center my-20">
-          <Gallery 
-            images={['/assets/49_Mk3-no-bg.png','/assets/61_Mk3-no-bg.png','/assets/88_Mk3-no-bg.png']}
+          <Gallery
+            images={['/assets/49_Mk3-no-bg.png', '/assets/61_Mk3-no-bg.png', '/assets/88_Mk3-no-bg.png']}
             titles={['Keylab 49 Mk3', 'Keylab 61 Mk3', 'Keylab 88 Mk3']}
-            links={['https://www.pcmidi.com.ar/search/?q=keylab49mk3','https://www.pcmidi.com.ar/search/?q=keylab61mk3#','https://www.pcmidi.com.ar/search/?q=keylab88mk3']}
+            links={['https://www.pcmidi.com.ar/search/?q=keylab49mk3', 'https://www.pcmidi.com.ar/search/?q=keylab61mk3#', 'https://www.pcmidi.com.ar/search/?q=keylab88mk3']}
           />
         </div>
-            <div className='w-full h-[40rem] relative overflow-hidden'>
-                <Image
-                    src="/assets/keylab-closeup.jpg"
-                    alt="Banner principal"
-                    fill
-                    className='object-cover'
-                    quality={100}
-                />
-            </div>
+        <div className='w-full h-[40rem] relative overflow-hidden'>
+          <Image
+            src="/assets/keylab-closeup.jpg"
+            alt="Banner principal"
+            fill
+            className='object-cover'
+            quality={100}
+          />
+        </div>
       </main>
       <footer className="w-full h-32 bg-black text-gray-50 flex items-center justify-center">
-          <div className="text-center text-md font-[supply-light]">
-            PC Midi Center distribuidor oficial de Arturia en Argentina
-          </div>
+        <div className="text-center text-md font-[supply-light]">
+          PC Midi Center distribuidor oficial de Arturia en Argentina
+        </div>
       </footer>
     </div>
   );
