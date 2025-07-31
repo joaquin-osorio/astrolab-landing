73f1a725778aa9eaf082a8c7740bdb4b58be349f
joaquin-osorio <joaquinosorio55@gmail.com>
2025-07-31 10:57:07 -0300

    Feat: Added Gallery.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,8 @@ import { keylab88Specs, keylab49And61Specs } from "@/data/specs";
 import Specifications from "@/components/Specifications";
 import VideoBanner from "@/components/VideoBanner";
 import video from "@/pu"
+import Gallery from "@/components/Gallery"
+
 
 export default function Home() {
   return (
@@ -25,6 +27,13 @@ export default function Home() {
         <VideoBanner src="/assets/render-loop.mp4" heightClass="h-[500px]"/>
         <Specifications title='Keylab 88 Mk3' specs={keylab88Specs}/>
         <Specifications title='Keylab 49/61 Mk3' specs={keylab49And61Specs}/>
+        <div className="w-full h-96 flex justify-center">
+          <Gallery 
+            images={['/assets/49_Mk3-no-bg.png','/assets/61_Mk3-no-bg.png','/assets/88_Mk3-no-bg.png']}
+            titles={['Keylab 49 Mk3', 'Keylab 61 Mk3', 'Keylab 88 Mk3']}
+            links={['#','#','#']}
+          />
+        </div>
       </main>
     </div>
   );
---
+++ src/components/Gallery.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Image from "next/image";
+
+export interface GalleryProps {
+  images: string[];
+  titles: string[];
+  links: string[];
+}
+
+export default function Gallery({
+  images,
+  titles,
+  links,
+}: GalleryProps): JSX.Element {
+  return (
+    <div className="flex w-[80vw] h-full bg-gray-50">
+        {images.map((src,i) => {
+          console.log(images[i])
+          return(
+            <a key={i} href={links[i]} className="h-full w-1/3">
+              <div className="">
+                <div className=" relative w-full h-64 transform scale-100 transition-transform duration-500 ease-out hover:scale-110">
+                  <Image 
+                    src={images[i]}
+                    alt={titles[i]}
+                    fill
+                    className="object-contain"
+                  />
+                </div>
+                <h3 className="ml-10 text-black text-md mt-4 font-[supply-bold]">{titles[i]}</h3>
+              </div>
+            </a>
+          )
+        })}
+    </div>
+  );
+}
